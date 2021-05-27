cc28393e2b19f7451ed288b7a72c1ab0eeaae098
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-05-26 21:54:36 -0400

    Starting Files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+  background-color: #141625;
+}
+</style>
